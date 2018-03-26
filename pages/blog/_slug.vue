272f40ae7b307d6dc256a14c1e41413939ff473f
J.R. Smith <johnathanraysmith2@gmail.com>
2018-03-26 18:53:04 -0400

    test _slug.vue

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,12 +1,19 @@
 <template>
   <article>
-    <h1>{{ title }}</h1>
+    <div class="hero-body">
+      <div class="container">
+        <div class="has-text-centered intro">
+          <h1 class="title is-size-1">{{ title }}</h1>
+        </div>
+      </div>
+    </div>
   </article>
 </template>
 
 <script>
 export default {
   async asyncData({ params }) {
+    console.log(params)
     // const postPromise = process.BROWSER_BUILD
     //   ? import('~/content/blog/posts/' + params.slug + '.json')
     //   : Promise.resolve(
@@ -14,6 +21,7 @@ export default {
     //     );
 
     let post = await import("~/content/blog/posts/" + params.slug + ".json")
+    console.log(post)
     return post
   }
 }
