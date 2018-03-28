1b3b55f42913de417e55a8d7e70f1be62651ffa6
J.R. Smith <johnathanraysmith2@gmail.com>
2018-03-28 12:53:13 -0400

    change home form name to "contact"

--- pages/index.vue
+++ pages/index.vue
@@ -92,7 +92,7 @@ export default {
   data() {
     return {
       formAction: "contact",
-      formName: "contactJohnathan",
+      formName: "contact",
       form: {
         name: "",
         email: "",
